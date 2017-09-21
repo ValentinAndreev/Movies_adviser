94c11e197aac2c2a6838f0622406d7c6391adbfc
ValentinAndreev <valentinandreev80@gmail.com>
2017-09-21 11:39:02 +0300

    Added movies to ActveAdmin

---
+++ app/admin/movie.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Movie do
+  permit_params :title, :overview, :vote_average, :imbd_id, :tmdb_id, :release_date, :genres
+
+  index do
+    column :title
+    column :overview
+    column :vote_average
+    column :imbd_id
+    column :tmdb_id
+    column :release_date
+    column :genres    
+    actions
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+  validates :title, :overview, :poster_path, :vote_average, :imbd_id, :tmdb_id, :release_date, :genres, presence: true  
 end
