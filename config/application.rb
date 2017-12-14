bd36f197a278206fa134b53c120d005e1fa7371f
ValentinAndreev <valentinandreev80@gmail.com>
2017-12-14 09:59:54 +0300

    Reorganize PORO's locations

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module MoviesAdviser
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += [ "#{Rails.root}/app/services/queries" ]   
+    config.autoload_paths += Dir[ Rails.root.join('app', 'models', '**/') ]
+    config.autoload_paths += Dir[ Rails.root.join('app', 'controllers', '**/') ] 
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
